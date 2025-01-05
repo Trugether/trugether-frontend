9e6d9c9cc9aa7173df58a05790933bc60a0b187d
amani <n180447@rguktn.ac.in>
2025-01-05 15:10:42 +0530

    Fixed alignment in registration screen

---
+++ lib/registration/screens/registration_screen copy.dart
@@ -0,0 +1,153 @@
+// import 'package:flutter/material.dart';
+
+// class RegistrationScreen extends StatefulWidget {
+//   const RegistrationScreen({super.key});
+
+//   @override
+//   State<RegistrationScreen> createState() => _RegistrationScreenState();
+// }
+
+// class _RegistrationScreenState extends State<RegistrationScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       backgroundColor: Colors.black,
+//       body: Center(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: [
+//               Image.asset(
+//                 'assets/images/logo/logo.png',
+//                 height: 180,
+//                 width: 180,
+//               ),
+//               // SizedBox(height: 8),
+//               // Title
+//               Text(
+//                 "Trugether",
+//                 style: TextStyle(
+//                   fontSize: 28,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.white,
+//                 ),
+//                 textAlign: TextAlign.center,
+//               ),
+//               SizedBox(height: 6),
+//               // Subtitle
+//               Text(
+//                 "Register and join the music community!",
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(fontSize: 14, color: Colors.white70),
+//               ),
+//               SizedBox(height: 24),
+//               SizedBox(
+//                 height: 45,
+//                 child: TextField(
+//                   decoration: InputDecoration(
+//                     hintText: "Username",
+//                     hintStyle: TextStyle(color: Colors.grey),
+//                     contentPadding: EdgeInsets.symmetric(
+//                       vertical: 8,
+//                       horizontal: 20,
+//                     ),
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.all(Radius.circular(50.0)),
+//                     ),
+//                   ),
+//                   keyboardType: TextInputType.text,
+//                   style: TextStyle(color: Colors.white),
+//                 ),
+//               ),
+//               SizedBox(height: 14),
+//               SizedBox(
+//                 height: 45,
+//                 child: TextField(
+//                   decoration: InputDecoration(
+//                     hintText: 'Password',
+//                     hintStyle: TextStyle(color: Colors.grey),
+//                     contentPadding: EdgeInsets.symmetric(
+//                       vertical: 8,
+//                       horizontal: 20,
+//                     ),
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.all(Radius.circular(50.0)),
+//                     ),
+//                   ),
+//                   obscureText: true,
+//                   style: TextStyle(color: Colors.white),
+//                 ),
+//               ),
+//               SizedBox(height: 14),
+//               SizedBox(
+//                 height: 45,
+//                 child: TextField(
+//                   decoration: InputDecoration(
+//                     hintText: 'Confirm Password',
+//                     hintStyle: TextStyle(color: Colors.grey),
+//                     contentPadding: EdgeInsets.symmetric(
+//                       vertical: 8,
+//                       horizontal: 20,
+//                     ),
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.all(Radius.circular(50.0)),
+//                     ),
+//                   ),
+//                   obscureText: true,
+//                   style: TextStyle(color: Colors.white),
+//                 ),
+//               ),
+//               SizedBox(height: 16),
+//               SizedBox(
+//                 height: 45,
+//                 child: ElevatedButton(
+//                   onPressed: () {
+//                     // Handle registration logic
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(50.0),
+//                     ),
+//                     backgroundColor: Colors.green,
+//                     foregroundColor: Colors.white,
+//                     elevation: 5,
+//                   ),
+//                   child: Text(
+//                     'Register',
+//                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(height: 16),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Text(
+//                     "Already have an account? ",
+//                     style: TextStyle(color: Colors.white),
+//                   ),
+//                   GestureDetector(
+//                     onTap: () {
+//                       Navigator.pop(context);
+//                     },
+//                     child: Text(
+//                       'Login',
+//                       style: TextStyle(
+//                         color: Colors.blue,
+//                         decoration: TextDecoration.underline,
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
--- lib/registration/screens/registration_screen.dart
+++ lib/registration/screens/registration_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trugether_frontend/login/screens/login_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
@@ -13,139 +14,194 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.black,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset(
-                'assets/images/logo/logo.png',
-                height: 180,
-                width: 180,
-              ),
-              // SizedBox(height: 8),
-              // Title
-              Text(
-                "Trugether",
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 6),
-              // Subtitle
-              Text(
-                "Register and join the music community!",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 14, color: Colors.white70),
-              ),
-              SizedBox(height: 24),
-              SizedBox(
-                height: 45,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Username",
-                    hintStyle: TextStyle(color: Colors.grey),
-                    contentPadding: EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 20,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                    ),
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: 0.1,
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: List.generate(
+                6,
+                (index) => Container(
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[700],
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  keyboardType: TextInputType.text,
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              SizedBox(height: 14),
-              SizedBox(
-                height: 45,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    hintStyle: TextStyle(color: Colors.grey),
-                    contentPadding: EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 20,
+                  child: Center(
+                    child: Image.asset(
+                      'assets/images/grid/image${index + 1}.jpg',
+                      fit: BoxFit.cover,
+                      height: 150,
                     ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                    ),
-                  ),
-                  obscureText: true,
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              SizedBox(height: 14),
-              SizedBox(
-                height: 45,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Confirm Password',
-                    hintStyle: TextStyle(color: Colors.grey),
-                    contentPadding: EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 20,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                    ),
-                  ),
-                  obscureText: true,
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              SizedBox(height: 16),
-              SizedBox(
-                height: 45,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Handle registration logic
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                    ),
-                    backgroundColor: Colors.green,
-                    foregroundColor: Colors.white,
-                    elevation: 5,
-                  ),
-                  child: Text(
-                    'Register',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Text(
-                    "Already have an account? ",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        decoration: TextDecoration.underline,
+                  const SizedBox(height: 80), // To offset from the top
+                  // Fixed Header Section
+                  Column(
+                    children: [
+                      Image.asset(
+                        'assets/images/logo/logo.png',
+                        height: 180,
+                        width: 180,
                       ),
-                    ),
+                      Text(
+                        "Trugether",
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 6),
+                      Text(
+                        "Register and join the music community!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 14, color: Colors.white70),
+                      ),
+                      SizedBox(height: 24),
+                    ],
+                  ),
+                  // Input Fields Section
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                        height: 45,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(50.0),
+                              ),
+                            ),
+                            hintText: "Username",
+                            hintStyle: TextStyle(color: Colors.grey),
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 8,
+                              horizontal: 20,
+                            ),
+                          ),
+                          keyboardType: TextInputType.text,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(height: 14),
+                      SizedBox(
+                        height: 45,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            hintStyle: TextStyle(color: Colors.grey),
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 8,
+                              horizontal: 20,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(50.0),
+                              ),
+                            ),
+                          ),
+                          obscureText: true,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(height: 14),
+                      SizedBox(
+                        height: 45,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Confirm Password',
+                            hintStyle: TextStyle(color: Colors.grey),
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 8,
+                              horizontal: 20,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(50.0),
+                              ),
+                            ),
+                          ),
+                          obscureText: true,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      SizedBox(
+                        height: 45,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Handle login logic
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                50.0,
+                              ), // Match TextField's border radius
+                            ),
+                            // Adjust padding for size
+                            backgroundColor:
+                                Colors.green, // Button background color
+                            foregroundColor: Colors.white, // Text color
+                            elevation: 5, // Add shadow for a raised effect
+                          ),
+                          child: Text(
+                            'Register',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ), // Customize text style
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Already have an account? ",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginScreen(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
